00dba0e29204cf6a37c739dd8b30842408179b4c
coffeecoder3peat <philip.c.wilson@outlook.com>
2019-02-15 21:55:52 -0700

    update for fixes and added out file capability

--- Compare-Text.ps1
+++ Compare-Text.ps1
@@ -8,7 +8,11 @@ function Compare-Configuration
 
         [Parameter(Mandatory = $true)]
         [string]
-        $DifferenceConfigPath
+        $DifferenceConfigPath,
+
+        [Parameter()]
+        [AllowNull()]
+        $OutputPath
     )
 
     $referenceConfig = Get-Content -Path $ReferenceConfigPath
@@ -22,9 +26,17 @@ function Compare-Configuration
     $textDifference = Compare-Object -ReferenceObject $referenceConfig -DifferenceObject $differenceConfig -IncludeEqual
 
     # Compare objects with line numbers and sort by line number
-    $numberedTextDifference = Compare-Object -ReferenceObject $numberedReference -DifferenceObject $numberedDifference -IncludeEqual -Property Text,LineNum | 
-        sort-object -Property LineNum
+    $numberedTextDifference = Compare-Object -ReferenceObject $numberedReference -DifferenceObject $numberedDifference -IncludeEqual -Property Text,LineNum
 
+    foreach ($line in $numberedTextDifference)
+    {
+        if ($line.SideIndicator -eq '=>')
+        {
+            $line.LineNum = $line.LineNum - 1
+        }
+    }
+
+    $numberedTextDifference = $numberedTextDifference | Sort-Object -Property LineNum
     $newConfigurationParams = @{
         TextDifference             = $textDifference
         NumberedReference          = $numberedReference
@@ -32,7 +44,9 @@ function Compare-Configuration
         CombinedNumberedDifference = $numberedTextDifference
     }
 
-    Write-NewConfiguration @newConfigurationParams
+    $newConfiguration = Get-NewConfiguration @newConfigurationParams
+
+    Out-ConfigurationResult -Configuration $newConfiguration -OutputPath $OutputPath
     
 }
 
@@ -92,7 +106,7 @@ function Collect-Whatever
     return $returnArray
 }
 
-function Write-NewConfiguration
+function Get-NewConfiguration
 {
     param
     (
@@ -136,42 +150,112 @@ function Write-NewConfiguration
         }
     }
 
-    
+    $finalCollection = [System.Collections.ArrayList]::new()
+    $diffCollection = [System.Collections.ArrayList]::new()
     $lineCount = 1
     foreach ($line in $CombinedNumberedDifference)
     {
         switch ($line.Text) 
         {
             {$PSItem -eq $equalArray[0].InputObject} 
-            {  
+            {
+                if ($diffCollection.Count -gt 0)
+                {
+                    [int] $byte = 97 # Lowercase A
+                    foreach ($diffLine in $diffCollection)
+                    {
+                        $diffLineCount = "$($lineCount - 1).$([CHAR][BYTE]$byte)"
+                        $diffString = "[{0}]::Difference::`t{1}" -f $diffLineCount, $diffLine
+                        $byte ++
+
+                        $null = $finalCollection.Add($diffString)
+                    }
+
+                    $diffCollection.Clear()
+                }
+                
                 $strLine = $line.Text
                 $strLine = "$strLine".replace("`t"," ")
-                "[{0}]`t{1}" -f $line.LineNum, $strLine | Write-Output
+                $equalString = "[{0}]`t{1}" -f $lineCount, $strLine 
 
+                $null = $finalCollection.Add($equalString)
                 $null = $equalArray.Remove($equalArray[0])
+
                 break
             }
             {$PSItem -eq $referenceArray[0].InputObject}
             {
-                "Reference[{0}]`t{1}" -f $lineCount, $line.Text | Write-Host -BackgroundColor Green -ForegroundColor Black
+                if ($diffCollection.Count -gt 0)
+                {
+                    [int] $byte = 97 # Lowercase A
+                    foreach ($diffLine in $diffCollection)
+                    {
+                        $diffLineCount = "$($lineCount - 1).$([CHAR][BYTE]$byte)"
+                        $diffString = "[{0}]::Difference::`t{1}" -f $diffLineCount, $diffLine
+                        $byte ++
 
+                        $null = $finalCollection.Add($diffString)
+                    }
+
+                    $diffCollection.Clear()
+                }
+
+                $referenceString = "[{0}]::Reference::`t{1}" -f $lineCount, $line.Text
+                $null = $finalCollection.Add($referenceString)
                 $null = $referenceArray.Remove($referenceArray[0])
                 break
             }
             {$PSItem -eq $differenceArray[0].InputObject}
             {
-                $lineCount = $lineCount - 1
-                "Difference[{0}]`t{1}" -f $lineCount, $line.Text | Write-Host -BackgroundColor Red -ForegroundColor Black
-                
+                $null = $diffCollection.Add($line.Text)
                 $null = $differenceArray.Remove($differenceArray[0])
-                break
+
+                $lineCount = $lineCount - 1
             }
         }
 
         $lineCount++
     }
 
-    #foreach ($line in $NumberedReference)
+    return $finalCollection
+}
+
+function Out-ConfigurationResult
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        $Configuration,
+
+        [Parameter()]
+        [AllowNull()]
+        $OutputPath
+    )
+
+    foreach ($line in $Configuration)
+    {
+        switch ($line)
+        {
+            {$PSItem -match '.*::Reference::.*'}
+            {
+                Write-Host -Object $line -BackgroundColor Green -ForegroundColor Black
+                break
+            }
+            {$PSItem -match '.*::Difference::.*'}
+            {
+                Write-Host -Object $line -BackgroundColor Red -ForegroundColor Black
+            }
+            default
+            {
+                Write-Output -InputObject $line
+            }
+        }
+
+        if ($OutputPath)
+        {
+            Out-File -InputObject $line -FilePath $OutputPath -Append 
+        }
+    }
 }
 
 Compare-Configuration -ReferenceConfigPath 'C:\Users\phili\Desktop\Compare function\Config1.txt' -DifferenceConfigPath 'C:\Users\phili\Desktop\Compare function\Config2.txt'
